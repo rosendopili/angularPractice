5611a1b4912a0ee8da4e7e5e4b36ea489817a757
rosendopili <rosendopili@gmail.com>
2020-01-22 21:42:24 -0500

    static typing in typescript

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -5,10 +5,29 @@
 // var message = 'Hello World';
 //
 // log(message);
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-doSomething();
+//
+// function doSomething(){
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+//   console.log('Finally: ' + i);
+// }
+//
+// doSomething();
+var a;
+var b;
+var c;
+var d;
+var e; //array
+var f;
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+; //each element automatically gets an incremented value unless explicitly set  
+var backgroundColor = Color.Red;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -5,12 +5,35 @@
 // var message = 'Hello World';
 //
 // log(message);
+//
+// function doSomething(){
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+//   console.log('Finally: ' + i);
+// }
+//
+// doSomething();
 
-function doSomething(){
-  for (var i = 0; i < 5; i++) {
-    console.log(i);
-  }
-  console.log('Finally: ' + i);
+  // let a: number;
+  // let b: boolean;
+  // let c: string;
+  // let d: any;
+  // let e: number[]; //array
+  // let f: any[];
+  //
+  // const ColorRed = 0;
+  // const ColorGreen = 1;
+  // const ColorBlue = 2;
+  //
+  // enum Color { Red = 0, Green = 1, Blue = 2 }; //each element automatically gets an incremented value unless explicitly set
+  // let backgroundColor = Color.Red;
+
+  let message = 'abc';
+  let endsWithC = (<any>message).endsWith('c');
+
+let log = function(message) {
+  console.log(message);
 }
 
-doSomething();
+let doLog = () => console.log();

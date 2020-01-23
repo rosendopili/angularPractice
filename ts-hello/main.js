6d2357caf1371357a17806fb27e8157ffb6102d0
rosendopili <rosendopili@gmail.com>
2020-01-22 21:18:51 -0500

    type script compile demo

---
+++ ts-hello/main.js
@@ -0,0 +1,14 @@
+// function log(message) {
+//   console.log(message);
+// }
+//
+// var message = 'Hello World';
+//
+// log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
